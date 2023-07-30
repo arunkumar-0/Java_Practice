aace2c84ec4ef8f5d9e826a51eeabb5dfd496010
ARUN_KUMAR <gurukularunyadav@gmail.com>
2023-07-30 18:26:09 +0530

    complex pattern programming done

--- ComplexPattern.java
+++ ComplexPattern.java
@@ -1,3 +1,19 @@
+// printing pattern at diagonals
+
 public class ComplexPattern {
-    
+    public static void main(String[] args) {
+        int n = 19 ; 
+        int i , j ; 
+        for (i = 0 ; i<n ; i++){
+            for(j=0 ; j<n ; j++){
+                if(i==0||j==0||i==n-1||j==n-1||i==j||i+j==n-1||i+j==(n-1)/2|| i-j==(n-1)/2||j-i==(n-1)/2||i+j==(n-1)+(n-1)/2){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
+
