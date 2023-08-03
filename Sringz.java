9f0d63cd990c9f43ba49ee465f45e613496ed36d
ARUN_KUMAR <gurukularunyadav@gmail.com>
2023-08-03 23:40:56 +0530

    adding string topic

---
+++ Sringz.java
@@ -0,0 +1,12 @@
+// two types of strings : mutable strings and immutable strings
+
+class strinz{
+    public static void main(String[] args) {
+        String name = "arun";     //immutable 
+        System.out.println(name);
+
+        StringBuilder name1 = new StringBuilder("arun kumar");  //mutable
+        System.out.println(name1);
+        name1.append("kurukshetra");     
+    }
+}
