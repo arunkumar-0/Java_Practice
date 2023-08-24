bedd94198e2c1b2e36637281e54ddc38a6a45349
ARUN_KUMAR <gurukularunyadav@gmail.com>
2023-08-24 07:55:32 +0530

    updated constructor

--- Encapsulation/Cnstr1.java
+++ Encapsulation/Cnstr1.java
@@ -1,27 +1,30 @@
 package Encapsulation;
-class Demo {
-    private int a ; 
-    private int b ; 
-   public Demo ()
-    {
+
+class Demo1 {
+    private int a;
+    private int b;
+
+    public Demo1() {
         System.out.println("0 parameter constructor");
     }
-   public Demo(int a , int b){
-        this.a = a ; 
-        this.b = b ; 
+
+    public Demo1(int a, int b) {
+        this.a = a;
+        this.b = b;
     }
-    void show()
-    {
+
+    void show() {
         System.out.println(a);
         System.out.println(b);
     }
 }
+
 public class Cnstr1 {
     public static void main(String[] args) {
-        Demo obj = new Demo();
+        Demo1 obj = new Demo1();
         obj.show();
 
-        Demo obj1 = new Demo(10 , 15);
+        Demo1 obj1 = new Demo1(10, 15);
         obj1.show();
     }
 }
